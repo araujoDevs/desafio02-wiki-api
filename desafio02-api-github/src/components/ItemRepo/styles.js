c2d7f270dd82f38bb71c821dc19d6ee2922f3734
AraujOliveira <araujo448@outlook.com>
2025-01-24 13:00:09 -0300

    Desfio02-wiki-api-repo

---
+++ desafio02-api-github/src/components/ItemRepo/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    widt: 80%;
+
+    h3{
+        font-size: 32px;
+        font-weight: bold;
+        color: #FAFAFA;
+        margin-bottom:20px;
+    }
+
+    p{
+        font-size: 18px;
+        color: #FAFAFA60;
+        margin-bottom:20px;
+    }
+
+    a.remover {
+        color: #FF0000;
+    }
+    
+    .link{
+    text-decoration: none;
+    color: #FFFFFF;
+    }
+
+    hr {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+`
